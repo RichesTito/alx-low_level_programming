d5c2bddc6618f6580c2a7d85fc289b195aad6ffe
RichesTito <olakunleriches@gmail.com>
2022-10-13 08:32:28 -0700

    and the piece of art is useful

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
-#include <stdio.h>
-
+#include <unistd.h>
 /**
+ *
  * main - Prints a text in C
  *
  * Return: Always 1 (Successful)
+ *
  */
+
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }

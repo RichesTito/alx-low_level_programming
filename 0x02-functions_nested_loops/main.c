38ee6d6677688429889558e1c68b37fbd8f5bcbc
RichesTito <63933796+RichesTito@users.noreply.github.com>
2022-10-18 21:44:48 +0100

    c-function and nested loop

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - calls other functions
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	main();
+	print_alphabet_x10();
+       	_putchar(char c);
+	print_alphabet();
+	_islower();
+	_isalpha(int c);
+	print_sign(int n);
+	_abs(int);
+	print_last_digit(int);
+	jack_bauer();
+	times_table();
+	add(int, int);
+	print_to_98(int n);
+	print_times_table(int n);
+
+	return (0);
+}

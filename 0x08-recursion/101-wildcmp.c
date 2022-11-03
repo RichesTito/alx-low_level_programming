fa1d2500f411a8a03cc945d809fda6250f44bddb
RichesTito <olakunleriches@gmail.com>
2022-11-03 07:28:07 -0700

    task 101

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * wildcmp - compares two strings.
  * @s1: string 1.
@@ -13,7 +14,7 @@ int wildcmp(char *s1, char *s2)
 		return (1);
 	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
-	if (*s2 == '')
+	if (*s2 == '*')
 		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 	return (0);
 }
